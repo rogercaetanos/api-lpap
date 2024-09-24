979f7da0b88c32276aaffd463b8907d77915f6d4
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2024-09-24 18:37:04 -0300

    Início Controle de versão

---
+++ src/main/java/com/itb/inf2bn/apilpap/ApiLpapApplication.java
@@ -0,0 +1,31 @@
+package com.itb.inf2bn.apilpap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLpapApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiLpapApplication.class, args);
+
+		/*
+
+		Tarefa:
+Enviar projetos para o github
+
+1- Criar um projeto
+2- Abrir qualquer terminal dentro da pasta do projeto
+3- Criar o repositório git ( git init )
+4- Preparar os arquivos que serão controlados
+(git add "nome de cada arquivo")
+Dica: Adicionar todos de um vez
+(git add .)
+Obs: A qualquer momento podemos conferir o status
+(git status)
+
+		 */
+	}
+
+}
